0e94c3f4f5cedb335210c8e767a2b5c8fda70d3e
dmarsua7-stack <dmarsua7@gmail.com>
2026-04-14 19:30:45 +0100

    Creación del proyecto y commit inicial

---
+++ src/test/java/com/informaticatelde/gestordeportivo/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informaticatelde.gestordeportivo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
